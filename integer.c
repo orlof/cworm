a1624ceb052780d723ec1be6c82f11f985d96ef1
Teppo Koskinen <teppo.koskinen@omenia.fi>
2016-02-08 01:11:04 +0200

    kjkjh

---
+++ integer.c
@@ -0,0 +1,9 @@
+//
+// Created by Teppo Koskinen on 08/02/16.
+//
+
+#include "memory.h"
+
+int int_cmp(REF lref, REF rref) {
+    return 0;
+}
---
+++ string.c
@@ -0,0 +1,9 @@
+//
+// Created by Teppo Koskinen on 08/02/16.
+//
+
+#include "memory.h"
+
+int str_cmp(REF lref, REF rref) {
+    return 0;
+}
